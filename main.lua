4e4f50e3336bd7d96789124c76ae90ee8ab87d79
greenya <greenyadzer@gmail.com>
2019-08-03 22:21:02 +0300

    Spell tooltip

--- main.lua
+++ main.lua
@@ -10,34 +10,41 @@ frame:SetScript("OnEvent", function (self, event, ...)
 end)
 
 local get_entry = function (type, id)
-    print("lookup for " .. type .. " #" .. id)
-    -- todo: item look up db[type][id], where type "item", "spell", "quest" etc.
-    return {
-        "Назва предмета",
-        "Певний текст українською мовою який репрезентує переклад для цього предмета."
-    }
+    if type and id then
+        print("lookup for " .. type .. " #" .. id)
+        -- todo: item look up db[type][id], where type "item", "spell", "quest" etc.
+        return {
+            "Назва предмета",
+            "Певний текст українською мовою який репрезентує переклад для цього предмета."
+        }
+    end
+    return false
 end
 
-local OnTooltipSetItem = function (tt, ...)
-    local entry = false
-
-    local _, link = tt:GetItem()
-    if link then
-        local _, _, id = link:find("Hitem:(%d+):")
-        if id then
-            entry = get_entry("item", id)
-        end
-    end
-
+local add_entry_to_tooltip = function (type, id, tt)
+    local entry = get_entry(type, id)
     if entry then
         tt:AddLine(" ")
         tt:AddLine("|TInterface\\AddOns\\ClassicUA\\ua:0|t " .. entry[1], 1, 1, 1)
         tt:AddLine(entry[2], 1, 1, 1, true)
-        tt:AddLine(" ")
     end
+end
+
+local tt_set_item = function (tt, ...)
+    local _, link = tt:GetItem()
+    if link then
+        local _, _, id = link:find("Hitem:(%d+):")
+        add_entry_to_tooltip("item", id, tt)
+    end
+end
+
+local tt_set_spell = function (tt, ...)
+    local _, id = tt:GetSpell()
+    add_entry_to_tooltip("spell", id, tt)
 end
 
 
 for _, tt in pairs { GameTooltip } do
-    tt:HookScript("OnTooltipSetItem", OnTooltipSetItem)
+    tt:HookScript("OnTooltipSetItem", tt_set_item)
+    tt:HookScript("OnTooltipSetSpell", tt_set_spell)
 end
