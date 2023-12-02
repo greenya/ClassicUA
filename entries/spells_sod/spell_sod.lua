c1fb68a449b871d4408cf5ba97414a8389ff083a
Yevhenii Kozii <varmid@gmail.com>
2023-12-02 12:32:41 +0200

    Add SoD spells

---
+++ entries/spells_sod/spell_sod.lua
@@ -0,0 +1,15 @@
+local _, addonTable = ...
+
+local spells_sod = {
+
+-- [id] = {
+--     [ref] = ID (optional),
+--     [1] = title (optional),
+--     [2] = description (optional),
+--     [3] = aura (optional),
+-- }
+
+-- Building Inspiration
+[408953] = { "Сповнення натхненням", nil, "Сповнюється натхненням." }, -- Building Inspiration
+
+}
