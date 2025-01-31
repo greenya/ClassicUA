c29349079a80493229335bc781b41715d40f9b5f
Yevhenii Kozii <varmid@gmail.com>
2025-01-31 15:27:09 +0200

    Update items

--- entries/wrath/item.lua
+++ entries/wrath/item.lua
@@ -3,13 +3,13 @@ local items = {
 
 -- See /entries/classic/item.lua for data format details.
 
-[7906] = { "роги Нез'ри" }, -- Horns of Nez'ra
-[9594] = { "зменшений звір" }, -- Beast Muisek
-[9618] = { "посудина зменшення звірів", use="Зменшити та захопити мертвого звіра." }, -- Beast Muisek Vessel
-[9628] = { "мішечок трав Зераша" }, -- Xerash's Herb Pouch
-[11368] = { "указ Рінна", flavor="Скріплено печаткою Штормовію" }, -- Wrynn's Decree
-[13506] = { "зілля скам'яніння", use=17624 }, -- Potion of Petrification
-[50432] = { "шкура хворого вовка" }, -- Diseased Wolf Pelt
+[7906] = { "роги Нез'ри", en="Horns of Nez'ra" },
+[9594] = { "зменшений звір", en="Beast Muisek" },
+[9618] = { "посудина зменшення звірів", use="Зменшити та захопити мертвого звіра.", en="Beast Muisek Vessel" },
+[9628] = { "мішечок трав Зераша", en="Xerash's Herb Pouch" },
+[11368] = { "указ Рінна", flavor="Скріплено печаткою Штормовію", en="Wrynn's Decree" },
+[13506] = { "зілля скам'яніння", use=17624, en="Potion of Petrification" },
+[50432] = { "шкура хворого вовка", en="Diseased Wolf Pelt" },
 }
 
 if addonTable.item then
